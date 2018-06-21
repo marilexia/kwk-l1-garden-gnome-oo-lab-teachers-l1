18701de7c4f1e0f7d6e854383a7b7d4a37582414
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-21 00:29:25 +0000

    Done.

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,25 @@
 # Code your instances here
+class GardenGnome
+  attr_accessor :name, :age, :gluten_allergy, :personality
+
+
+  def initialize=(name, ages, personality)
+    @name = "name"
+    @ages = "ages"
+    @personality = "evil"
+  end
+
+  def name
+    @name
+  end
+
+  def ages
+    @ages
+  end
+
+  def personality
+    "evil"
+  end
+
+
+end
