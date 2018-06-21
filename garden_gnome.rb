584be5ff4b2f6d7926eb0bc8e48533557bd2a51e
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-21 01:53:03 +0000

    Done.

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -39,4 +39,16 @@ class GardenGnome
     @gluten_allergy
   end
 
+  def gnaw
+    return "Gnawing on a tree!!!"
+  end
+
+  def shout
+    return "GNARLY!!!"
+  end
+
+  def introduce_self
+    return "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
+  end
+
 end
